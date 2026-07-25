21e79a180a5cd91d4623a112ad5502933594178b
wakai <wakai39@gmail.com>
2026-07-26 06:38:49 +0900

    テーブル再作成のマイグレを消して、laravel初期設定で作成されるマイグレに入れ替える

--- database/migrations/0001_01_01_000001_create_cache_table.php
+++ database/migrations/0001_01_01_000001_create_cache_table.php
@@ -14,7 +14,13 @@ return new class extends Migration
         Schema::create('cache', function (Blueprint $table) {
             $table->string('key')->primary();
             $table->mediumText('value');
-            $table->integer('expiration')->index();
+            $table->bigInteger('expiration')->index();
+        });
+
+        Schema::create('cache_locks', function (Blueprint $table) {
+            $table->string('key')->primary();
+            $table->string('owner');
+            $table->bigInteger('expiration')->index();
         });
     }
 
@@ -24,5 +30,6 @@ return new class extends Migration
     public function down(): void
     {
         Schema::dropIfExists('cache');
+        Schema::dropIfExists('cache_locks');
     }
 };
